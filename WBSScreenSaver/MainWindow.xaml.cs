9b6b915ad06796bffa8cc46b7a2efbd624981416
NerdyDev <id@nerdy.place>
2022-08-30 11:06:52 +0300

    oled-screensaver

--- WBSScreenSaver/MainWindow.xaml.cs
+++ WBSScreenSaver/MainWindow.xaml.cs
@@ -1,67 +1,52 @@
-﻿#region copyright
-// Copyright (c) 2015 Wm. Barrett Simms wbsimms.com
-//
-// Permission is hereby granted, free of charge, to any person 
-// obtaining a copy of this software and associated documentation 
-// files (the "Software"), to deal in the Software without restriction, including 
-// without limitation the rights to use, copy, modify, merge, publish, 
-// distribute, sublicense, and/or sell copies of the Software, 
-// and to permit persons to whom the Software is furnished to do so, 
-// subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be 
-// included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
-// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
-// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-#endregion
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
-namespace WBSScreenSaver
+namespace OLEDScreensaver
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		public MainWindow()
 		{
 			InitializeComponent();
-			this.Loaded += MainWindow_Loaded;
+			Loaded += MainWindow_Loaded;
 		}
 
 		void MainWindow_Loaded(object sender, RoutedEventArgs e)
 		{
-			TransformGroup group = new TransformGroup();
-			double width = this.MainGrid.RenderSize.Width;
-			DoubleAnimation animation = new DoubleAnimation((width / 2) * -1, width / 2 + logo.ActualWidth, new Duration(new TimeSpan(0, 0, 0, 10)));
-			animation.RepeatBehavior = RepeatBehavior.Forever;
-			TranslateTransform tt = new TranslateTransform(-logo.ActualWidth * 2, 0);
-			logo.RenderTransform = group;
-			logo.Width = 200;
-			logo.Height = 200;
-			group.Children.Add(tt);
-			tt.BeginAnimation(TranslateTransform.XProperty, animation);
-			WindowState = WindowState.Maximized;
+
+			double width = MainGrid.RenderSize.Width, 
+				   height = MainGrid.RenderSize.Height, 
+				   vmin = Math.Min(width, height), 
+				   logoSize = vmin / 8.4375,
+				   sizeUnit = logoSize / 8;
+
+			logo.Width = logo.Height = logoSize;
+
+            time.FontSize = sizeUnit * 8;
+            time.Margin = new Thickness(0, 0, sizeUnit * 8, sizeUnit * 10);
+            time.Height = sizeUnit * 12;
+
+            date.FontSize = sizeUnit * 4;
+            date.Margin = new Thickness(0, 0, sizeUnit * 8, sizeUnit * 5);
+            date.Height = sizeUnit * 8;
+
+			var timer = new DispatcherTimer();
+			timer.Interval = new TimeSpan(0, 0, 1);
+			timer.Tick += delegate { SyncTime(); };
+			timer.Start();
+
+			SyncTime();
+
+            WindowState = WindowState.Maximized;
 			Mouse.OverrideCursor = Cursors.None;
+		}
+
+		void SyncTime()
+		{
+			date.Content = DateTime.Now.ToLongDateString();
+			time.Content = DateTime.Now.ToShortTimeString();
 		}
 
 		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
